32a141bf0d8095fcf61722c91e66df4c0640f62a
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2021-09-23 16:58:24 -0300

    Add components do the main App file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header /> 
+    <app-content />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    AppContent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    background: #1d1f2b;
+    color: #c7ccd8;
+    padding: 0;
+    margin: 0;
+    border: 0;
+    text-decoration: none;
+  }
 </style>

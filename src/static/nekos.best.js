8c8dbb571e9d24a328e69c2863b4f84808c5173b
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2021-09-23 17:00:10 -0300

    Add static js file w/ an array of strings

---
+++ src/static/nekos.best.js
@@ -0,0 +1,32 @@
+const gifs = [
+  'baka​',
+  'bite​',
+  'blush​',
+  'bored​',
+  'cry​',
+  'cuddle​',
+  'dance​',
+  'facepalm​',
+  'feed​',
+  'happy​',
+  'highfive​',
+  'hug​',
+  'kiss​',
+  'laugh​',
+  'pat​',
+  'poke​',
+  'pout​',
+  'shrug​',
+  'slap​',
+  'sleep​',
+  'smile​',
+  'smug​',
+  'stare​',
+  'think​',
+  'thumbsup​',
+  'tickle​',
+  'wave​',
+  'wink​'
+]
+
+export default gifs

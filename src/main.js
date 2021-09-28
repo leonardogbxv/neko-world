084885057d97ec1404b13211a6ee2284a9a08e97
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2021-09-28 17:01:01 -0300

    Add anime.js lib

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import anime from 'animejs/lib/anime.es.js'
 
 Vue.prototype.$axios = axios
+Vue.prototype.$anime = anime
 
 new Vue({
   render: h => h(App),
